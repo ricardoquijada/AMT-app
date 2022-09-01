2fe28410a4a982539c881984e4773e72041da754
Riqui <ricardoquijadaa@gmail.com>
2022-09-01 07:13:55 -0600

    modification

--- app-test.py
+++ app-test.py
@@ -18,7 +18,7 @@ def skill_columns(skill):
     general_info = list_columns[1:9]
     cols_to_use = general_info
     if skill == "Systems":
-        cols_to_use.extend(list_columns[10:22])
+        cols_to_use.extend(list_columns[9:22])
         cols_to_use.extend(list_columns[24:40])
     if skill == "Avionics":
         cols_to_use.extend(list_columns[41:53])
@@ -39,6 +39,46 @@ def total(skill):
         total_surveys = answer_df[answer_df["Skill"]
                                   == skill_selected].shape[0]
     return total_surveys
+
+
+def validation(code):
+    if code in full_df["Code 86"].tolist():
+        if code in answer_df["Code 86"].tolist():
+            st.success('Complete')
+            return True
+        else:
+            st.warning("Pending to survey")
+            return False
+    else:
+        st.error("Not survey required")
+        return False
+
+
+def profile_sheet(code):
+    data = answer_df[(answer_df["Code 86"]
+                      == code)]  # Esta línea saca el dataset del colaborador
+    # Determina que columnas usar según la skill
+    name = full_df["NAME"][full_df["Code 86"] == code].to_list()[0]
+    email = data["E-mail"].to_list()[0]
+    role = data["Current Role"].to_list()[0]
+    skill = data["Skill"].to_list()[0]
+    phone = str(data["Phone number"].to_list()[0])
+    academic_level = data["Academic level"].to_list()[0]
+    visa = data["VISA"].to_list()[0]
+    cols_used = skill_columns(skill)
+    st.write("## 👓 General Information")
+    general_info = {
+        "Field": ["Name", "E-mail", "Current Role", "Skill", "Phone Number", "Academic Level", "Visa"],
+        "Description": [name, email, role, skill, phone, academic_level, visa]
+    }
+    st.table(general_info)
+    technical_df = answer_df[cols_used][(
+        answer_df["Code 86"] == code)]
+    technical_info = {
+        "Subskill": list(technical_df.columns)[8:],
+        "Description": list(technical_df.iloc[0, 8:])
+    }
+    st.table(technical_info)
 
 
 answer_sheet = "1Uwhn3yj7SfCRJO1LACrOJ30Q6zDraV4BhUK6i6U2DPw"
@@ -70,13 +110,10 @@ list_columns = answer_df.columns.to_list()
 # ---- Header ----
 st.image("./img/aeroman7371.jpg", width=250)
 st.write("# AMT Subskill Overview")
-Verification, Overview = st.tabs(["Verification", "Overview"])
-with Verification:
-    code_86 = st.text_input("Code 86")
-    if code_86 in full_df["Code 86"].tolist():
-        st.success('Complete')
-    else:
-        st.warning("Pending to survey")
+Overview, Profile, Verification = st.tabs(
+    ["Overview", "Profile", "Verification"])
+# ------ Tab Distribution -------
+
 with Overview:
     total_surveys = total(skill_selected)
     st.write(f"  ###  🎯 Total Surveys - {skill_selected}: {total_surveys}")
@@ -87,20 +124,20 @@ with Overview:
     x = answer_df.value_counts(["Skill", "Business Unit"])
     with col1:
         if skill_selected == "All":
-            count_1 = answer_df.value_counts(["Business Unit"])["U1"][0]
+            count_1 = answer_df.value_counts(["Business Unit"])["U1"]
         else:
             count_1 = x[skill_selected]["U1"]
         st.header(f"U1: {count_1}")
 
     with col2:
         if skill_selected == "All":
-            count_2 = answer_df.value_counts(["Business Unit"])["U2"][0]
+            count_2 = answer_df.value_counts(["Business Unit"])["U2"]
         else:
             count_2 = x[skill_selected]["U2"]
         st.header(f"U2 : {count_2}")
     with col3:
         if skill_selected == "All":
-            count_3 = answer_df.value_counts(["Business Unit"])["U3"][0]
+            count_3 = answer_df.value_counts(["Business Unit"])["U3"]
         else:
             count_3 = x[skill_selected]["U3"]
         st.header(f"U3: {count_3}")
@@ -130,5 +167,17 @@ with Overview:
         st.plotly_chart(Role_fig)
         x = answer_df.groupby("VISA").count()
         Visa_fig = px.pie(x, values="E-mail", names=x.index, color=x.index,
-                          color_discrete_sequence=px.colors.sequential.RdBu, title="VISA", hole=0.3)
+                          color_discrete_sequence=px.colors.sequential.Darkmint, title="VISA", hole=0.3)
         st.plotly_chart(Visa_fig)
+
+with Verification:
+    code_86 = st.text_input("Code 86")
+    status = validation(code_86)
+
+with Profile:
+    profile_code = st.text_input("Insert 86")
+    status_profile = validation(profile_code)
+    if status_profile:
+        profile_sheet(profile_code)
+    else:
+        st.write("### No information found 😞")
